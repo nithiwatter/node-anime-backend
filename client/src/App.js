1b3850d32eab1a31acc7ef2f7862abf97c208d0e
Nithiwat Seesillapachai <ns271@duke.edu>
2020-05-19 22:19:19 +0700

    clean project

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import AnimeGallery from './pages/AnimeGallery';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <CssBaseline></CssBaseline>
-        <AnimeGallery></AnimeGallery>
+        Hello
       </div>
     );
   }
